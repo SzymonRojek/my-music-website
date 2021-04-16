4ec97965595631d21e4edc62859bb353ebe2a0ae
Szymon Rojek <sz.rojek@gmail.com>
2021-04-16 22:33:41 +0100

    remove screen.width if statement

--- src/js/script.js
+++ src/js/script.js
@@ -3,32 +3,30 @@
 
 // navigation slide and links fade animation
 
-const hamburger = document.querySelector(".hamburger");
+const navHamburger = document.querySelector(".nav__hamburger");
 const navList = document.querySelector(".nav__list");
 const navLinks = document.querySelectorAll(".nav__link");
 
 function fadeLinks() {
   navLinks.forEach( (link, index) => {
-    if(window.screen.width < 767) {
+
       link.style.animation = link.style.animation ? 
       "" : link.style.animation = `linksFade .5s ease forwards ${index / 5 + .5}s`;
-    }
+
   })
 }
 
 // hamburger animation and open nav on click with links fading 
-hamburger.addEventListener("click", () => {
-
-  hamburger.classList.toggle("js-active");
+navHamburger.addEventListener("click", () => {
+  navHamburger.classList.toggle("js-active");
   navList.classList.toggle('nav__list--open');
-
   fadeLinks();
 
   // change the state true or false
-  if(hamburger.classList.contains("js-active")) {
-    hamburger.setAttribute("aria-expanded", true);
+  if(navHamburger.classList.contains("js-active")) {
+    navHamburger.setAttribute("aria-expanded", true);
   } else {
-    hamburger.setAttribute("aria-expanded", false);
+    navHamburger.setAttribute("aria-expanded", false);
   }
 })
 
@@ -38,7 +36,7 @@ hamburger.addEventListener("click", () => {
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     navList.classList.remove('nav__list--open');
-    hamburger.classList.remove('js-active');
+    navHamburger.classList.remove('js-active');
     fadeLinks();
   })
 })
