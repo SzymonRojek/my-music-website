36237f277ebfc80bf7d4222550f5f0ba754292db
Szymon Rojek <sz.rojek@gmail.com>
2021-04-16 16:54:14 +0100

    smooth scroll js between links

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,65 @@
 
+function smoothScroll(tar, duration) {
+  const target = document.querySelector(tar);
+  const targetPosition = target.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  console.log(targetPosition);
+
+  function ease(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2* t* t + b;
+    t--;
+    return -c/2 * (t * (t-2) - 1) + b;
+  }
+
+  function animation(currentTime) {
+    if(startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if(timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  requestAnimationFrame(animation);
+}
 
 
- 
+// scroll to home section
+const homeLink = document.getElementById("homeLink");
+
+homeLink.addEventListener("click", () => {
+  smoothScroll(".scroll-home", 1500);
+})
+
+
+// scroll to intro section 
+const introLink = document.getElementById("introLink");
+
+introLink.addEventListener("click", () => {
+  smoothScroll(".intro", 1500);
+})
+
+
+// scroll to feedback section
+const feedbackLink = document.getElementById("feedbackLink");
+
+feedbackLink.addEventListener("click", () => {
+  smoothScroll(".feedback", 1500);
+})
+
+// scroll from the bottom to the top of the page
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  smoothScroll(".scroll-home", 1500);
+});
+
+
+
 // navigation slide and links fade animation
 
 const hamburger = document.querySelector(".hamburger");
@@ -16,6 +74,7 @@ function fadeLinks() {
   })
 }
 
+// hamburger animation and open nav on click with links fading 
 hamburger.addEventListener("click", () => {
 
   hamburger.classList.toggle("js-active");
@@ -31,7 +90,8 @@ hamburger.addEventListener("click", () => {
   }
 })
 
-// scroll between links => hide the nav bar on mobile
+
+// links => hide the nav bar when a link is clicked 
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
