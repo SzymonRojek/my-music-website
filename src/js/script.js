16f75a9ea46a9cb96d2507f8b830442b264100d2
Szymon Rojek <sz.rojek@gmail.com>
2021-04-16 16:01:45 +0100

    add scroll between links, hide navar by using js - still working with scroll. I have to include the height of the navbar as a top scroll edge

--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,27 @@
 
 
-const section2 = document.querySelector('.section-two');
-
-function smoothScroll(tar, duration) {
-  const target = document.querySelector(tar);
-  const targetPosition = target.getBoundingClientRect().top;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  let startTime = null;
-
-
-  function ease(t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2* t* t + b;
-    t--;
-    return -c/2 * (t * (t-2) - 1) + b;
-  }
-
-  function animation(currentTime) {
-    if(startTime === null) startTime = currentTime;
-    const timeElapsed = currentTime - startTime;
-    const run = ease(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run);
-    if(timeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  requestAnimationFrame(animation);
-}
- 
-
-
-const goUp = () => {
-  smoothScroll('.scroll-top', 1500);
-}
-
-section2.addEventListener('click', goUp);
-
-
 
  
-// hamburger 
+// navigation slide and links fade animation
 
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav__list");
 const navLinks = document.querySelectorAll(".nav__link");
 
-
-hamburger.addEventListener("click", () => {
-
-  hamburger.classList.toggle("js-active");
-  navList.classList.toggle('nav__list--open');
- 
-
+function fadeLinks() {
   navLinks.forEach( (link, index) => {
     
     link.style.animation = link.style.animation ? 
     "" : link.style.animation = `linksFade .5s ease forwards ${index / 5 + .5}s`;
   })
+}
 
+hamburger.addEventListener("click", () => {
+
+  hamburger.classList.toggle("js-active");
+  navList.classList.toggle('nav__list--open');
+
+  fadeLinks();
 
   // change the state true or false
   if(hamburger.classList.contains("js-active")) {
@@ -65,4 +29,14 @@ hamburger.addEventListener("click", () => {
   } else {
     hamburger.setAttribute("aria-expanded", false);
   }
+})
+
+// scroll between links => hide the nav bar on mobile
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('nav__list--open');
+    hamburger.classList.remove('js-active');
+    fadeLinks();
+  })
 })
