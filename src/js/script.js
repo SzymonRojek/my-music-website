c04a6e0a1d041f5c396ff0fe1b058b85d3de7b97
Szymon Rojek <sz.rojek@gmail.com>
2021-04-17 23:32:04 +0100

    working with BEM, edit intro and feddback sections

--- src/js/script.js
+++ src/js/script.js
@@ -3,44 +3,42 @@
 
 // navigation slide and links fade animation
 
-const navHamburger = document.querySelector(".navigation__button");
-const navList = document.querySelector(".navigation__list");
-const navLinks = document.querySelectorAll(".navigation__link");
+const navigationSwitcher = document.querySelector(".navigation__button");
+const navigationList = document.querySelector(".navigation__list");
+const navigationLinks = document.querySelectorAll(".navigation__link");
 
 
 function fadeLinks() {
-  navLinks.forEach( (link, index) => {
-    const animationLink = `linksFade .5s ease forwards ${index / 5 + .5}s`;
-      if(window.innerWidth <= 767 && !link.style.animation) {
-        link.style.animation = animationLink;
-      } else {
-        link.style.animation = '';
-      }
+  navigationLinks.forEach( (link, index) => {
+    if(window.innerWidth <= 767 && !link.classList.contains('linksFade')) {
+      link.classList.add('linksFade');
+    } else {
+      link.classList.remove('linksFade');
+    }
   })
 }
 
 // hamburger animation and open nav on click with links fading 
-navHamburger.addEventListener("click", () => {
-  navHamburger.classList.toggle("js-active");
-  navList.classList.toggle('navigation__list--open');
+navigationSwitcher.addEventListener("click", () => {
+  navigationSwitcher.classList.toggle("js-active");
+  navigationList.classList.toggle('navigation__list--open');
   fadeLinks();
 
   // change the state true or false
-  if(navHamburger.classList.contains("js-active")) {
-    navHamburger.setAttribute("aria-expanded", true);
+  if(navigationSwitcher.classList.contains("js-active")) {
+    navigationSwitcher.setAttribute("aria-expanded", true);
   } else {
-    navHamburger.setAttribute("aria-expanded", false);
+    navigationSwitcher.setAttribute("aria-expanded", false);
   }
 })
 
 
 // links => hide the nav bar when a link is clicked 
-
 function resetStates() {
-  navLinks.forEach(link => {
+  navigationLinks.forEach(link => {
     link.addEventListener('click', () => {
-      navList.classList.remove('nav__list--open');
-      navHamburger.classList.remove('js-active');
+      navList.classList.toggle('navigation__list--open');
+      navHamburger.classList.toggle("js-active");
       fadeLinks();
     })
   })
