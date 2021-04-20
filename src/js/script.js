08f565546e7a0010d432e0e7bfa812b5b7fe2f97
Szymon Rojek <sz.rojek@gmail.com>
2021-04-20 22:31:29 +0100

    change logic js, add IIFE, use strict

--- src/js/script.js
+++ src/js/script.js
@@ -1,69 +1,67 @@
 
 
-const switcherNavigationHamburger = document.querySelector(".navigation__hamburgerButton");
-const switcherNavigationButton = document.querySelector(".navigation__list-arrowButton");
-const navigationList = document.querySelector(".navigation__list");
-const navigationLinks = document.querySelectorAll(".navigation__link");
+(() => {
 
+  "use strict"
 
-function fadeLinks() {
-  navigationLinks.forEach( (link, index) => {
-    if(window.innerWidth <= 767 && !link.classList.contains('linksFade')) {
-      link.classList.add('linksFade');
-    } else {
-      link.classList.remove('linksFade');
-    }
-  })
-}
-
-// hamburger animation and open nav on click with links fading 
-switcherNavigationHamburger.addEventListener("click", (e) => {
-  switcherNavigationHamburger.classList.toggle("js-active");
-  navigationList.classList.toggle('navigation__list--open');
-  fadeLinks();
-
-  if(navigationList.classList.contains("navigation__list--open")) {
-    switcherNavigationHamburger.setAttribute("aria-expanded", true);
-
-  } else {
-    switcherNavigationHamburger.setAttribute("aria-expanded", false);
-  }
-
-  if(switcherNavigationHamburger.getAttribute('aria-expanded') === true) {
-    switcherNavigationHamburger.classList.remove('js-active')
-  }
-
-})
-
-switcherNavigationButton.addEventListener("click", () => {
-  navigationList.classList.toggle('navigation__list--open');
-
-  if(switcherNavigationHamburger.classList.contains('js-active')) {
-    switcherNavigationHamburger.classList.remove('js-active');
-  }
-
-  if(navigationList.classList.contains("navigation__list--open")) {
-    switcherNavigationButton.setAttribute("aria-expanded", true);
-  } else {
-    switcherNavigationButton.setAttribute("aria-expanded", false);
-  }
-
- 
-  fadeLinks();
-
+  const switcherNavigationHamburger = document.querySelector(".navigation__hamburgerButton");
+  const switcherNavigationButton = document.querySelector(".navigation__list-arrowButton");
+  const navigationList = document.querySelector(".navigation__list");
+  const navigationLinks = document.querySelectorAll(".navigation__link");
   const arrowRight = '>';
   const arrowLeft = '<';
-  switcherNavigationButton.innerText = switcherNavigationButton.innerText === arrowRight ? arrowLeft : arrowRight;
-})
 
-// links => hide the nav bar when a link is clicked 
-function resetStates() {
-  navigationLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      navigationList.classList.remove('navigation__list--open');
-      switcherNavigationHamburger.classList.remove("js-active");
-      fadeLinks();
+
+  function fadeLinks() {
+    navigationLinks.forEach( (link, index) => {
+      if(window.innerWidth <= 767 && !link.classList.contains('linksFade')) {
+        link.classList.add('linksFade');
+      } else {
+        link.classList.remove('linksFade');
+      }
     })
+  }
+
+  // hamburger animation and open nav on click with links fading 
+  switcherNavigationHamburger.addEventListener("click", () => {
+    switcherNavigationHamburger.classList.toggle("js-active");
+    navigationList.classList.toggle('navigation__list--open');
+    fadeLinks();
+
+    if(navigationList.classList.contains("navigation__list--open")) {
+      switcherNavigationHamburger.setAttribute("aria-expanded", true);
+    } else {
+      switcherNavigationHamburger.setAttribute("aria-expanded", false);
+    }
   })
-}
-resetStates();
+
+  switcherNavigationButton.addEventListener("click", () => {
+    navigationList.classList.toggle('navigation__list--open');
+
+    if(switcherNavigationHamburger.classList.contains('js-active')) {
+      switcherNavigationHamburger.classList.remove('js-active');
+    }
+
+    if(navigationList.classList.contains("navigation__list--open")) {
+      switcherNavigationButton.setAttribute("aria-expanded", true);
+    } else {
+      switcherNavigationButton.setAttribute("aria-expanded", false);
+    }
+
+    fadeLinks();
+    switcherNavigationButton.innerText = switcherNavigationButton.innerText === arrowRight ? arrowLeft : arrowRight;
+  })
+
+  // links => hide the nav bar when a link is clicked 
+  function resetStates() {
+    navigationLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        navigationList.classList.remove('navigation__list--open');
+        switcherNavigationHamburger.classList.remove("js-active");
+        fadeLinks();
+      })
+    })
+  }
+  resetStates();
+
+})();
