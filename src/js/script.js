ea74819008e654990099a1b58b1957883d36fa5c
Szymon Rojek <sz.rojek@gmail.com>
2021-04-23 20:02:35 +0100

    edit html file

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 (() => {
 
   'use strict';
-  
+
   const navigationHamburgerButton = document.querySelector('.navigation__hamburgerButton');
   const navigationArrowButton = document.querySelector('.navigation__list-arrowButton');
   const navigationList = document.querySelector('.navigation__list');
@@ -10,8 +10,8 @@
   const arrowLeft = '<';
 
   function fadeLinks() {
-    navigationLinks.forEach( link => {
-      if ( window.innerWidth <= 767 && !link.classList.contains('linksFade') ) {
+    navigationLinks.forEach(link => {
+      if (window.innerWidth <= 767 && !link.classList.contains('linksFade')) {
         link.classList.add('linksFade');
       } else {
         link.classList.remove('linksFade');
@@ -19,26 +19,26 @@
     })
   }
 
-  navigationHamburgerButton.addEventListener( 'click', () => {
+  navigationHamburgerButton.addEventListener('click', () => {
     navigationHamburgerButton.classList.toggle('js-active');
     navigationList.classList.toggle('navigation__list--open');
     fadeLinks();
 
-    if ( navigationList.classList.contains('navigation__list--open') ) {
+    if (navigationList.classList.contains('navigation__list--open')) {
       navigationHamburgerButton.setAttribute('aria-expanded', true);
     } else {
       navigationHamburgerButton.setAttribute('aria-expanded', false);
     }
   })
 
-  navigationArrowButton.addEventListener( 'click', () => {
+  navigationArrowButton.addEventListener('click', () => {
     navigationList.classList.toggle('navigation__list--open');
 
-    if ( navigationHamburgerButton.classList.contains('js-active') ) {
+    if (navigationHamburgerButton.classList.contains('js-active')) {
       navigationHamburgerButton.classList.remove('js-active');
     }
 
-    if ( navigationList.classList.contains('navigation__list--open') ) {
+    if (navigationList.classList.contains('navigation__list--open')) {
       navigationHamburgerButton.classList.add('js-active');
       navigationArrowButton.setAttribute('aria-expanded', true)
     } else {
@@ -51,8 +51,8 @@
   })
 
   function resetStates() {
-    navigationLinks.forEach( link => {
-      link.addEventListener( 'click', () => {
+    navigationLinks.forEach(link => {
+      link.addEventListener('click', () => {
         navigationList.classList.remove('navigation__list--open');
         navigationHamburgerButton.classList.remove('js-active');
         fadeLinks();
@@ -60,35 +60,35 @@
     })
   }
   resetStates();
- 
-  window.addEventListener( 'scroll', () => {
-    if ( window.pageYOffset > 200 ) {
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 200) {
       navigationArrowButton.classList.add('navigation__list-arrowButton--active');
     } else {
       navigationArrowButton.classList.remove('navigation__list-arrowButton--active');
-    } 
+    }
   })
 
- // mode switcher functionality:
+  // mode switcher functionality:
   const body = document.querySelector('body').classList;
   const modeSwitcher = document.querySelector('#mode-switcher');
- 
+
   // check user preferences scheme color and set to dark or light
-  const userPrefersDarkMode = window.matchMedia( '(prefers-color-scheme: dark)' ).matches;
-    console.log(`Dark mode is ${userPrefersDarkMode? '🌒 on' : '☀️ off'}.`);
+  const userPrefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  console.log(`Dark mode is ${userPrefersDarkMode ? '🌒 on' : '☀️ off'}.`);
 
   userPrefersDarkMode ? body.add('dark') : body.remove('dark');
 
-  if ( body.contains('dark') ) {
+  if (body.contains('dark')) {
     modeSwitcher.checked = true;
     modeSwitcher.classList.add('checkbox--active');
   }
 
- modeSwitcher.addEventListener( 'click', () => {
-  const boolean = document.querySelector('#mode-switcher:checked') !== null;
+  modeSwitcher.addEventListener('click', () => {
+    const boolean = document.querySelector('#mode-switcher:checked') !== null;
     boolean ? body.add('dark') : body.remove('dark');
     modeSwitcher.classList.add('checkbox--active');
- })
+  })
 
 })();
 
