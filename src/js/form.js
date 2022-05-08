84e77b9814c13edbdd77e7c97917d23c8a4fdf89
Szymon Rojek <sz.rojek@gmail.com>
2022-05-09 00:40:57 +0100

    add form functionality, connect form to email js

---
+++ src/js/form.js
@@ -0,0 +1,60 @@
+const $form = document.querySelector(".form");
+const $submitFormButton = document.querySelector(".js-submitButton");
+
+init();
+
+function init() {
+  $form.addEventListener("submit", onFormSubmit);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { name, subject, email, description } = Object.fromEntries(
+    new FormData($form).entries()
+  );
+
+  const templateParams = {
+    name,
+    subject,
+    email,
+    description,
+  };
+
+  sendMessage(templateParams);
+}
+
+function sendMessage(config) {
+  emailjs
+    .send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, config)
+    .then((response) => {
+      if (response.status === 200) {
+        showSuccessModal();
+      }
+    })
+    .catch((error) => showErrorModal(error));
+}
+
+function showSuccessModal() {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Email has been sent",
+    showConfirmButton: false,
+    timer: 2000,
+    color: "#2c2323",
+  });
+}
+
+showErrorModal({ error: "something went wrong" });
+
+function showErrorModal(error) {
+  Swal.fire({
+    icon: "error",
+    title: "Email has not been sent",
+    text: `${
+      error.text ? error.text : "Something went wront, please send it again"
+    }`,
+    confirmButtonColor: "crimson",
+  });
+}
