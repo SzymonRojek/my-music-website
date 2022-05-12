6f894380accabe687cf8dc315cf25f70d40110f7
Szymon Rojek <sz.rojek@gmail.com>
2022-05-13 00:21:38 +0100

    remove code indicates to arrow button which has been removed

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,5 @@
 const $navigation = document.querySelector(".navigation");
 const $hamburgerButton = document.querySelector(".navigation__hamburgerButton");
-const $arrowButton = document.querySelector(".navigation__list-arrowButton");
 const $navigationLinkList = document.querySelector(".navigation__list");
 const $navigationLinks = document.querySelectorAll(".navigation__link");
 
@@ -9,7 +8,6 @@ init();
 function init() {
   $navigation.addEventListener("wheel", preventScroll, { passive: false });
   $navigation.addEventListener("click", (event) => setNavigationAction(event));
-  window.addEventListener("scroll", setActionArrowButtonOnScroll);
 }
 
 function preventScroll(event) {
@@ -26,14 +24,11 @@ function setNavigationAction(event) {
   $navigationLinkList.classList.toggle("navigation__list--open");
 
   fadeLinks();
-  setDirectionArrowButton();
 
   if ($navigationLinkList.classList.contains("navigation__list--open")) {
     $hamburgerButton.setAttribute("aria-expanded", true);
-    $arrowButton.setAttribute("aria-expanded", true);
   } else {
     $hamburgerButton.setAttribute("aria-expanded", false);
-    $arrowButton.setAttribute("aria-expanded", false);
   }
 }
 
@@ -44,17 +39,4 @@ function fadeLinks() {
       ? link.classList.add("linksFade")
       : link.classList.remove("linksFade")
   );
-}
-
-function setDirectionArrowButton() {
-  const arrowRight = ">";
-  const arrowLeft = "<";
-  $arrowButton.textContent =
-    $arrowButton.textContent === arrowRight ? arrowLeft : arrowRight;
-}
-
-function setActionArrowButtonOnScroll() {
-  window.pageYOffset > 200
-    ? $arrowButton.classList.add("navigation__list-arrowButton--active")
-    : $arrowButton.classList.remove("navigation__list-arrowButton--active");
 }
