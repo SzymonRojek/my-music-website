94f36f33cd0496ef932ccaaf929dac776f182646
Szymon Rojek <sz.rojek@gmail.com>
2022-05-13 19:18:04 +0100

    work with observer - add changing color links navigation

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,52 +1,55 @@
-const header = document.querySelector("nav");
-const sectionOne = document.querySelector(".hero-container");
-const heading = document.querySelector(
+const $navigation = document.querySelector(".navigation");
+const $heroContainer = document.querySelector(".hero-container");
+const $mainHeading = document.querySelector(
   ".navigation__container-content-heading"
 );
-const colorWhiteSmoke = "rgb(245, 245, 245)";
+const $navigationLinks = document.querySelectorAll(".navigation__link");
 
+const colorWhiteSmoke = "rgb(245, 245, 245)";
 const sectionOneOptions = {
   rootMargin: "-80px 0px 0px 0px",
 };
 
-const sectionOneObserverBackground = new IntersectionObserver(function (
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      header.classList.add("header-scrolled");
-    } else {
-      header.classList.remove("header-scrolled");
-    }
-  });
-},
-sectionOneOptions);
-
-sectionOneObserverBackground.observe(sectionOne);
-
-const sectionOneObserverColor = new IntersectionObserver(function (
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      const actualColor = getPropertyValue(
-        ".main-container",
-        "background-color"
-      );
-
-      if (actualColor === colorWhiteSmoke) {
-        heading.classList.add("heading-white");
+const sectionOneObserverBackground = new IntersectionObserver(
+  (entries, sectionOneObserver) =>
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        $navigation.classList.add("observer-nav-background");
+      } else {
+        $navigation.classList.remove("observer-nav-background");
       }
-    } else {
-      heading.classList.remove("heading-white");
-    }
-  });
-},
-sectionOneOptions);
+    }),
+  sectionOneOptions
+);
 
-sectionOneObserverColor.observe(sectionOne);
+sectionOneObserverBackground.observe($heroContainer);
+
+const sectionOneObserverColor = new IntersectionObserver(
+  (entries, sectionOneObserver) =>
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        const actualColor = getPropertyValue(
+          ".main-container",
+          "background-color"
+        );
+
+        if (actualColor === colorWhiteSmoke) {
+          $mainHeading.classList.add("observer-nav-color");
+          $navigationLinks.forEach((link) => {
+            link.classList.add("observer-nav-color");
+          });
+        }
+      } else {
+        $mainHeading.classList.remove("observer-nav-color");
+        $navigationLinks.forEach((link) => {
+          link.classList.remove("observer-nav-color");
+        });
+      }
+    }),
+  sectionOneOptions
+);
+
+sectionOneObserverColor.observe($heroContainer);
 
 function getPropertyValue(selector, property) {
   const element = document.querySelector(selector);
