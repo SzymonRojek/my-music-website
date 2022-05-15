3732f60c570649111a3fd99df629bd75651d161a
Szymon Rojek <sz.rojek@gmail.com>
2022-05-15 23:22:45 +0100

    add togglerMode to the observer

--- src/js/footerButtonObserver.js
+++ src/js/footerButtonObserver.js
@@ -1,13 +1,18 @@
 const $footer = document.querySelector(".footer");
 const $scrollToTopButton = document.querySelector(".footer__buttonUp");
+const $togglerMode = document.querySelector(".footer__buttonToggle");
 const $rootElement = document.documentElement;
 
 function toggleObserverCallback(entries, observer) {
-  entries.forEach((entry) =>
-    entry.isIntersecting
-      ? $scrollToTopButton.classList.add("footer__buttonUp--show")
-      : $scrollToTopButton.classList.remove("footer__buttonUp--show")
-  );
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      $scrollToTopButton.classList.add("footer__buttonUp--show");
+      $togglerMode.classList.add("footer__buttonToggle--show");
+    } else {
+      $scrollToTopButton.classList.remove("footer__buttonUp--show");
+      $togglerMode.classList.remove("footer__buttonToggle--show");
+    }
+  });
 }
 
 const footerObserver = new IntersectionObserver(toggleObserverCallback);
