95fcdc66aead062dae15d380ebbc20f41cebf157
Szymon Rojek <sz.rojek@gmail.com>
2022-06-01 19:36:18 +0100

    rename methods, edit FieldsValidationFormData - now there is a way to get method from templateValidation depends on formData from event on change and event on submit

--- src/js/form/FieldsValidationFormData.js
+++ src/js/form/FieldsValidationFormData.js
@@ -1,22 +1,27 @@
 class ValidationMethods {
-  static isLengthValid(input, errorMessage) {
+  static isRequired(input, errorMessage) {
     return input.length > 0 || { message: errorMessage };
   }
 
-  static isMinLength(input, min, errorMessage) {
+  static isLessThan(input, min, errorMessage) {
     return input.length >= min || { message: errorMessage };
   }
 
-  static isOverMaxLength(input, max, errorMessage) {
+  static isGreaterThan(input, max, errorMessage) {
     return input.length <= max || { message: errorMessage };
   }
 
-  static isEmailValid(input, errorMessage) {
-    const regexPattern =
+  static isNumber(input, errorMessage) {
+    const isNumberRegex = /\d/;
+    return !isNumberRegex.test(input) || { message: errorMessage };
+  }
+
+  static isEmail(input, errorMessage) {
+    const regexEmailPattern =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return (
-      regexPattern.test(String(input).toLowerCase()) || {
+      regexEmailPattern.test(String(input).toLowerCase()) || {
         message: errorMessage,
       }
     );
@@ -24,50 +29,71 @@ class ValidationMethods {
 }
 
 export class FieldsValidationFormData {
-  getErrors({ name, email, subject, description }) {
-    return {
-      name: [
-        ValidationMethods.isLengthValid(name, "name is required"),
-        ValidationMethods.isMinLength(name, 3, "must be at least 3 characters"),
-        ValidationMethods.isOverMaxLength(
-          name,
-          15,
-          "must be maximum 15 characters"
-        ),
-      ],
-      email: [
-        ValidationMethods.isLengthValid(email, "email is required"),
-        ValidationMethods.isMinLength(
-          email,
-          3,
-          "must be at least 3 characters"
-        ),
-        ValidationMethods.isEmailValid(
-          email,
-          "email is not valid - has to contains @ and ."
-        ),
-      ],
-      subject: [
-        ValidationMethods.isLengthValid(subject, "subject is required"),
-        ValidationMethods.isMinLength(
-          subject,
-          3,
-          "must be at least 3 characters"
-        ),
-        ValidationMethods.isOverMaxLength(
-          subject,
-          15,
-          "must be maximum 15 characters"
-        ),
-      ],
-      description: [
-        ValidationMethods.isLengthValid(description, "description is required"),
-        ValidationMethods.isMinLength(
-          description,
-          3,
-          "must be at least 3 characters"
-        ),
-      ],
+  static getErrors(formData) {
+    const templateValidation = {
+      name(name) {
+        return [
+          ValidationMethods.isRequired(name, "name is required"),
+          ValidationMethods.isNumber(name, "number is not required"),
+          ValidationMethods.isLessThan(
+            name,
+            3,
+            "must be at least 3 characters"
+          ),
+          ValidationMethods.isGreaterThan(
+            name,
+            15,
+            "must be maximum 15 characters"
+          ),
+        ];
+      },
+      email(email) {
+        return [
+          ValidationMethods.isRequired(email, "email is required"),
+          ValidationMethods.isLessThan(
+            email,
+            3,
+            "must be at least 3 characters"
+          ),
+          ValidationMethods.isEmail(
+            email,
+            "email is not valid - has to contains @ and ."
+          ),
+        ];
+      },
+      subject(subject) {
+        return [
+          ValidationMethods.isRequired(subject, "subject is required"),
+          ValidationMethods.isLessThan(
+            subject,
+            3,
+            "must be at least 3 characters"
+          ),
+          ValidationMethods.isGreaterThan(
+            subject,
+            15,
+            "must be maximum 15 characters"
+          ),
+        ];
+      },
+      description(description) {
+        return [
+          ValidationMethods.isRequired(description, "description is required"),
+          ValidationMethods.isLessThan(
+            description,
+            3,
+            "must be at least 3 characters"
+          ),
+        ];
+      },
     };
+
+    return [formData].reduce((acc, fieldsData) => {
+      for (const field in fieldsData) {
+        acc[field] = templateValidation[field](fieldsData[field]);
+      }
+
+      return acc;
+    }, {});
   }
 }
